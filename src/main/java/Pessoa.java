35d02ecb12043cab0e79c2c932f61c44816562ca
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-02-04 14:21:40 -0300

    cria classe inicial pessoa

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,12 @@
+public abstract class Pessoa {
+    private String nome;
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
