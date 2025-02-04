2d8aadd3ee9219ca3d2cdcf2fd7472b2e2da7d98
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-02-04 15:35:01 -0300

    finaliza curso inicial

--- src/main/java/Curso.java
+++ src/main/java/Curso.java
@@ -27,5 +27,74 @@ public class Curso {
 
     //fim get set constructor padrao
 
+    //relacionado a turmas
 
+    public void addTurma(Turma turma) {
+        if (this.turmas.contains(turma)) {
+            throw new IllegalArgumentException("Turma ja esta cadastrada");
+        }
+        this.turmas.add(turma);
+    }
+
+    public void removeTurma(Turma turma) {
+        this.turmas.remove(turma);
+    }
+
+    public Boolean turmaIsPresent(Turma turma) {
+        return this.turmas.contains(turma);
+    }
+
+    public ArrayList<String> getProfessores() {
+        ArrayList<String> professores = new ArrayList<>();
+        for (Turma turma : this.turmas) {
+            professores.add(turma.getProfessorNome());
+        }
+        return professores;
+    }
+
+    public ArrayList<String> getDisciplinas() {
+        ArrayList<String> disciplinas = new ArrayList<>();
+        for (Turma turma : this.turmas) {
+            disciplinas.add(turma.getNomeDisciplina());
+        }
+        return disciplinas;
+    }
+
+    public ArrayList<String> getAlunosDisciplinas() {
+        ArrayList<String> alunosDisciplinas = new ArrayList<>();
+        for (Turma turma : this.turmas) {
+            ArrayList<String> alunoNome = turma.getNomesAlunos();
+            alunosDisciplinas.addAll(alunoNome);
+        }
+        return alunosDisciplinas;
+    }
+
+
+    // relacionado a alunos
+
+    public void matricularAluno(Aluno aluno) {
+        if (this.alunoIsPresentOnCurso(aluno)) {
+            throw new IllegalArgumentException("Aluno ja esta matriculado no curso.")
+        }
+        this.alunos.add(aluno);
+    }
+
+    public void desmatricularAluno(Aluno aluno) {
+        if (!this.alunoIsPresentOnCurso(aluno)) {
+            throw new IllegalArgumentException("Aluno nao encontrado no curso!");
+        }
+        this.alunos.remove(aluno);
+    }
+
+    public Boolean alunoIsPresentOnCurso(Aluno aluno) {
+        return this.alunos.contains(aluno);
+    }
+
+    public ArrayList<String> getNomesAlunosCurso() {
+        ArrayList<String> nomesAlunos = new ArrayList<>();
+        for (Aluno aluno : alunos) {
+            nomesAlunos.add(aluno.getNome());
+        }
+        return nomesAlunos;
+    }
 }
